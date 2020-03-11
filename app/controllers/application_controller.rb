9b5d49649f86b18a376d5b9092f45cc76820ed54
core-melt <core.melt21@gmail.com>
2020-03-11 14:11:54 +0900

    [Finish]Login SignUp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,16 @@ class ApplicationController < ActionController::Base
     user_path(current_user.id)
   end
 
+  #sign_out後のredirect先変更する。rootパスへ。rootパスはhome topを設定済み。
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
   protected
   def configure_permitted_parameters
     added_attrs = [ :email, :nickname, :password, :profile_image_id, :introduction, :sex, :spoiler_prevention ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
   end
 end
